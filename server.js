9b8985a31801925ca1fc2e77f1130c4811c019d0
laurak2628 <laurakatsman@gmail.com>
2026-09-24 14:17:02 -0500

    removed d

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const http = require("http")
 const hostname = "0.0.0.0";
 const port = 3000;
 
-const server = http.createServer ((req, res) => {d
+const server = http.createServer ((req, res) => {
     let method = req.method +  " ";
     let url = req.url + "\n\n";
     let headers = JSON.stringify (req.headers, null, 4);
