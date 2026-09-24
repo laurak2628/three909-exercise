d501a220f6e454bc0e15e1ba4bd965f3b4cfc8aa
laurak2628 <laurakatsman@gmail.com>
2026-09-24 17:38:11 -0500

    pic fix 3.0

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/api/getName', (req, res) => {
 
 app.get('/api/getImage', (req, res) => {
     res.set('Access-Control-Allow-Origin', '*');
-    res.sendFile("/library_homepage.jpg");
+    res.sendFile("./library_homepage.jpg");
 });
 
 app.listen(port, () =>{
