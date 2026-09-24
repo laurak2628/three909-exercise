8f931474fb7458fd9ad976733429a3dedc6e7fb7
laurak2628 <laurakatsman@gmail.com>
2026-09-24 17:12:20 -0500

    website updates

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 3000;
 
 app.get ("/", (req, res) =>{
-    res.status(200).send( "Hello, Express");
+    res.status(200).send( "Welcome to my Library site...more content to follow");
 });
 
 app.get('/api/getName', (req, res) => {
